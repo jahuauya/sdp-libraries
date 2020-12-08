85c155201c244b9294189610ed2ef71d870bc58f
jfloresmartinez12 <jfloresmartinez12@gmail.com>
2020-12-08 15:02:43 -0500

    Update unit_test.groovy

--- libraries/maven/unit_test.groovy
+++ libraries/maven/unit_test.groovy
@@ -1,7 +1,7 @@
 def call(){
     stage("Unit Test"){      
         try {
-            docker.image("maven").inside("-v ~/.m2:/root/.m2 -v /usr/share/maven/conf:/usr/share/maven/conf "){
+            docker.image("maven").inside("-v /root/.m2:/root/.m2 -v /usr/share/maven/conf:/usr/share/maven/conf "){
                 dir(config.pomDirectory){
                     sh "mvn test"
                 }
