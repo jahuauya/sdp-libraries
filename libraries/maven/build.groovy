d350e8bc6a8821461b93ae9ae23a1a0ae05e5b5c
jfloresmartinez12 <jfloresmartinez12@gmail.com>
2020-12-08 06:24:54 -0500

    Update build.groovy

--- libraries/maven/build.groovy
+++ libraries/maven/build.groovy
@@ -1,7 +1,7 @@
 def call(){
     stage("Build"){
         docker.image("maven").inside("-v /tmp/.m2:/root/.m2"){
-            sleep(300)
+            //sleep(300)
             dir(config.pomDirectory){
                 sh "mvn  clean package -DskipTests=true"
             }
