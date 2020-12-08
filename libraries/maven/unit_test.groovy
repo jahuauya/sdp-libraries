135071f3eb4965ddf6404f45b28de8e520a08891
jfloresmartinez12 <jfloresmartinez12@gmail.com>
2020-12-08 13:20:26 -0500

    Update unit_test.groovy

--- libraries/maven/unit_test.groovy
+++ libraries/maven/unit_test.groovy
@@ -1,7 +1,9 @@
 def call(){
     stage("Unit Test"){      
         try {
-            sh "mvn test"
+            dir(config.pomDirectory){
+                sh "mvn test"
+            }    
         } catch (ex) {
             unstable('Unit Test failed')
             currentBuild.result = 'UNSTABLE'
