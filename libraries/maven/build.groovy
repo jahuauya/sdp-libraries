ef5f1df07c63cfbbc9254fc16ecb2a6f1f6df8ad
jfloresmartinez12 <jfloresmartinez12@gmail.com>
2020-12-08 15:08:36 -0500

    Update build.groovy

--- libraries/maven/build.groovy
+++ libraries/maven/build.groovy
@@ -1,6 +1,6 @@
 def call(){
     stage("Build"){
-        docker.image("maven").inside("-v /root/.m2:/root/.m2 -v /usr/share/maven/conf:/usr/share/maven/conf "){
+        docker.image("maven").inside("-v /root/.m2:/root/.m2 -v /usr/share/maven/conf/setting.xml:/usr/share/maven/conf/setting.xml "){
             dir(config.pomDirectory){
                 sh "mvn  clean package -DskipTests=true -X"
             }
