2b499b16a525b0cd64c62ed6b6eda31d7a45708e
jfloresmartinez12 <jfloresmartinez12@gmail.com>
2020-12-08 20:16:53 -0500

    Update unit_test.groovy

--- libraries/maven/unit_test.groovy
+++ libraries/maven/unit_test.groovy
@@ -1,11 +1,11 @@
 def call(){
     stage("Unit Test"){      
         try {
-            docker.image("maven").inside("-v /root/.m2:/root/.m2 -v /usr/share/maven/conf/setting.xml:/usr/share/maven/conf/setting.xml "){
+            //docker.image("maven").inside("-v /root/.m2:/root/.m2 -v /usr/share/maven/conf/setting.xml:/usr/share/maven/conf/setting.xml "){
                 dir(config.pomDirectory){
                     sh "mvn test"
                 }
-            }
+            //}
         } catch (ex) {
             unstable('Unit Test failed')
             currentBuild.result = 'UNSTABLE'
