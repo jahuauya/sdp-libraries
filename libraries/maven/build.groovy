103242ec305cc90453f302ab882abd88fbfeaefe
jfloresmartinez12 <jfloresmartinez12@gmail.com>
2020-12-09 20:31:07 -0500

    Update build.groovy

--- libraries/maven/build.groovy
+++ libraries/maven/build.groovy
@@ -1,11 +1,10 @@
 def call(){
+    mvn_proxy="-Dhttp.proxyHost=10.226.14.40 -Dhttp.proxyPort=8080 -Dhttp.nonProxyHosts=localhost|127.0.0.1|10.226.12.45"
     stage("Build"){
-        sh "pwd"
-        docker.image("alirizasaral/maven-with-proxy").inside("-v /tmp/.m2:/root/.m2  -e PROXY_HOST=10.226.14.40 -e PROXY_PORT=8080 -e NO_PROXY=10.226.12.45 -e PROXY_PROTOCOL=http"){
-            sh "pwd"
+        //docker.image("alirizasaral/maven-with-proxy").inside("-v /tmp/.m2:/root/.m2  -e PROXY_HOST=10.226.14.40 -e PROXY_PORT=8080 -e NO_PROXY=10.226.12.45 -e PROXY_PROTOCOL=http"){
+          docker.image("maven").inside("-v /tmp/.m2:/root/.m2"){
             dir(config.pomDirectory){
-                sh "pwd"
-                sh "mvn  clean package -DskipTests=true"
+                sh "mvn  clean package -DskipTests=true ${mvn_proxy}"
             }
         }
     }
