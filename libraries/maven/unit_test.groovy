a7f3659784ef851eb52c69cbc45987aee579cbee
jfloresmartinez12 <jfloresmartinez12@gmail.com>
2020-12-09 21:01:26 -0500

    Update unit_test.groovy

--- libraries/maven/unit_test.groovy
+++ libraries/maven/unit_test.groovy
@@ -1,11 +1,12 @@
 def call(){
     stage("Unit Test"){      
         try {
-            //docker.image("alirizasaral/maven-with-proxy").inside("-v /tmp/.m2:/root/.m2  -e PROXY_HOST=10.226.14.40 -e PROXY_PORT=8080 -e NO_PROXY=10.226.12.45"){
+            docker.image("maven").inside("-v /tmp/.m2:/root/.m2"){
                 dir(config.pomDirectory){
-                    sh "mvn test"
+                    mvn_proxy="-Dhttp.proxyHost=10.226.14.40 -Dhttp.proxyPort=8080 -Dhttp.nonProxyHosts=10.226.12.45"
+                    sh "mvn test ${mvn_proxy} -X"
                 }
-            //}
+            }
         } catch (ex) {
             unstable('Unit Test failed')
             currentBuild.result = 'UNSTABLE'
