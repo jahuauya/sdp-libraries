9e5f56b2a80680b11eddea50bb702cc479b1d2cf
jfloresmartinez12 <jfloresmartinez12@gmail.com>
2020-12-09 22:47:03 -0500

    Update unit_test.groovy

--- libraries/maven/unit_test.groovy
+++ libraries/maven/unit_test.groovy
@@ -3,7 +3,7 @@ def call(){
         try {
             //docker.image("maven").inside("-v /tmp/.m2:/root/.m2"){
                 dir(config.pomDirectory){
-                    mvn_proxy="-Dhttp.proxyHost=10.226.14.40 -Dhttp.proxyPort=8080 -Dhttp.nonProxyHosts=10.226.12.45 -Dhttp.PROTOCOL=http"
+                    mvn_proxy="-Dhttp.proxyHost=10.226.14.40 -Dhttp.proxyPort=8080 -Dhttp.nonProxyHosts=10.226.12.45 -Dhttp.Protocol=https"
                     sh "mvn test ${mvn_proxy} "
                 }
             //}
