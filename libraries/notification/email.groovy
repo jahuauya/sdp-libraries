6b0db0d903e562f72f4952ed1a0fa33fdf0674df
jfloresmartinez12 <jfloresmartinez12@gmail.com>
2020-12-16 00:55:04 -0500

    Update email.groovy

--- libraries/notification/email.groovy
+++ libraries/notification/email.groovy
@@ -7,10 +7,12 @@ void call(context) {
         body: """
             <p>Pipeline Status: ${currentBuild.currentResult}</p>
             <p>Job: '${env.JOB_NAME} [${env.BUILD_NUMBER}]'</p>
-            <p>Library: ${context.library}
+            
             <p>Step: ${context.step}
             <p>Check console output at <a href='${env.BUILD_URL}'>${env.JOB_NAME}]</a></p>""",
         //recipientProviders: [[$class: 'DevelopersRecipientProvider'], culprits(), developers(), requestor(), brokenBuildSuspects(), brokenTestsSuspects(), upstreamDevelopers()],
         to: config.destination
     )
 }
+
+//<p>Library: ${context.library}
