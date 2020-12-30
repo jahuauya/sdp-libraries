53dc1b63a5772eabc5a6d2d82677d8ea11cf7243
jfloresmartinez12 <jfloresmartinez12@gmail.com>
2020-12-30 12:53:56 -0500

    Update build.groovy

--- libraries/gradle/build.groovy
+++ libraries/gradle/build.groovy
@@ -1,5 +1,13 @@
 def call(){
-    stage("build"){
-        echo "gradle.build"      
+    
+    stage("Build"){
+        echo "Nombre de la compañia: ${config.compania}"
+        //docker.image("maven").inside("-v /tmp/.m2:/root/.m2"){
+            dir(config.pomDirectory){
+                mvn_proxy="-Dhttp.proxyHost=10.226.14.40 -Dhttp.proxyPort=8080 -Dhttp.nonProxyHosts=10.226.12.45"
+                mvn_proxy=""
+                sh "mvn  clean package -DskipTests=true ${mvn_proxy}"
+            }
+        //}
     }
 }
